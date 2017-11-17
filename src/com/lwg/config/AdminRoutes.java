6fa9193e5316d93eaad1a1b1f8ea0cf6a9e01623
guo315648865 <315648865@qq.com>
2017-11-17 10:54:05 +0800

    test

--- src/com/lwg/config/AdminRoutes.java
+++ src/com/lwg/config/AdminRoutes.java
@@ -5,12 +5,13 @@ import com.lwg.examples.controller.AccountController;
 import com.lwg.examples.interceptor.AdminInterceptor;
 
 public class AdminRoutes extends Routes{
-
+	
 	@Override
 	public void config() {
 		this.setBaseViewPath("/examples");//设计controller返回时页面基本路劲，并不是controller的基本访问路劲；下面的add()方法第三个参数必须设计"/"，且controller返回时不带/才有效
 		addInterceptor(new AdminInterceptor());
 		add("/examples/account", AccountController.class,"/");
+		System.out.println("-----------");
 	}
 
 }
